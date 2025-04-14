dcdacec17a7aa730d6bd0ee1980b49ddd7fe9ffd
Math3usH <matheushenriquecalsavara@gmail.com>
2025-04-14 16:44:08 -0300

    criar api's e usar shellp

--- app/urls.py
+++ app/urls.py
@@ -7,34 +7,37 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet 
-from core.views import CategoriaViewSet
-from core.views import EditoraViewSet
-from core.views import AutorViewSet
-from core.views import LivroViewSet
+from core.views import (
+    CategoriaViewSet,
+    EditoraViewSet,
+    UserViewSet,
+    AutorViewSet,
+    LivroViewSet,
+)
 
 router = DefaultRouter()
 
-router.register(r'usuarios', UserViewSet, basename='usuarios')
-router.register(r'categoria', CategoriaViewSet, basename='categoria')
-router.register(r"editoras", EditoraViewSet, basename='editora')
-router.register(r"Autores", AutorViewSet, basename='autor')
+router.register(r"usuarios", UserViewSet, basename="usuarios")
+router.register(r"categoria", CategoriaViewSet, basename="categoria")
+router.register(r"editoras", EditoraViewSet, basename="editora")
+router.register(r"Autores", AutorViewSet, basename="autor")
+router.register(r"Liro", LivroViewSet, basename="")
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # OpenAPI 3
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        'api/swagger/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui',
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
     path(
-        'api/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc',
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
     # API
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
 ]
--- core/views/livro.py
+++ core/views/livro.py
@@ -2,9 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 
 from core.serializers import LivroSerializer
 
-from core.models import LivroSerializer
+from core.models import Livro
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
-    serializer_class = LivroSerializers
-
+    serializer_class = LivroSerializer
