b5d4742743bb80bca366ca611b36b0afdb60389b
Math3usH <matheushenriquecalsavara@gmail.com>
2025-04-15 14:28:19 -0300

    incluindo campo autores no model livro

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,15 +1,18 @@
-from django.db import models 
+from django.db import models
+
+from core.models.autor import Autor
 from .categoria import Categoria
 from .editora import Editora
+
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
-    quantidade = models.IntegerField(default=0,  null=True, blank=True)
+    quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
+    categoria = models.ForeignKey( Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True,)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
-
+    autores = models.ManyToManyField(Autor, related_name="livros", blank=True)
 
 
     def __str__(self):
